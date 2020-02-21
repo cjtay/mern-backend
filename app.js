7297d54f26da468615491304dfefffa8a4d3dd76
Tay CJ <cjtay@outlook.sg>
2020-02-21 11:20:59 +0800

    db setup and env setup

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -29,6 +30,13 @@ app.use((error, req, res, next) => {
     });
 });
 
-app.listen(5000);
-
-// updated repository name
+mongoose
+    .connect(
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-zziow.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+    )
+    .then(() => {
+        app.listen(5000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
